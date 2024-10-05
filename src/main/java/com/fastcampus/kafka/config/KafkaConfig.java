6f0f21cd8dcb0e4e63d8015d5e78a7da59ab6def
twocowsong <twocowsong@naver.com>
2024-10-05 21:37:24 +0900

    처리량 늘리기

--- src/main/java/com/fastcampus/kafka/config/KafkaConfig.java
+++ src/main/java/com/fastcampus/kafka/config/KafkaConfig.java
@@ -47,8 +47,7 @@ public class KafkaConfig {
 	) {
 		ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
 		factory.setConsumerFactory(consumerFactory);
-		// 리스너 컨테이너의 동시성을 설정 - 하나의 스레드만 사용하여 메시지를 처리
-		factory.setConcurrency(1);
+
 		// 수동 커밋으로 변경
 		factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
 		return factory;
@@ -65,6 +64,8 @@ public class KafkaConfig {
 		props.put(ConsumerConfig.ALLOW_AUTO_CREATE_TOPICS_CONFIG, "false");
 		// 컨슈머가 한번에 최대 몇개의 레코드를 POLL할지 설정 : 기본값인 500개로 설정
 		props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, ConsumerConfig.DEFAULT_MAX_POLL_RECORDS);
+		// 수정 커밋으로 변경
+		props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
 		return new DefaultKafkaConsumerFactory<>(props);
 	}
 
@@ -78,10 +79,9 @@ public class KafkaConfig {
 
 		// 배치 컨슈머를 사용으로 변경
 		factory.setBatchListener(true);
-		factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.BATCH);
+		// 수동모드
+		factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
 
-		// 리스너 컨테이너의 동시성을 설정 - 하나의 스레드만 사용하여 메시지를 처리
-		factory.setConcurrency(1);
 		return factory;
 	}
 
--- src/main/java/com/fastcampus/kafka/consumer/MyConsumer.java
+++ src/main/java/com/fastcampus/kafka/consumer/MyConsumer.java
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static com.fastcampus.kafka.model.Topic.MY_JSON_TOPIC;
 
@@ -20,10 +22,14 @@ public class MyConsumer {
 
 	private final Map<String, Integer> idHistoryMap = new ConcurrentHashMap<>();
 	private final ObjectMapper objectMapper = new ObjectMapper();
+	// 비동기로 메시지를 처리할 친구
+	private final ExecutorService executorService = Executors.newFixedThreadPool(10);
+
 	@KafkaListener(
 		topics = {MY_JSON_TOPIC},
 		groupId = "test-consumer-group",
-		containerFactory = "kafkaListenerContainerFactory"
+		containerFactory = "kafkaListenerContainerFactory",
+		concurrency = "3"
 	)
 	public void accept(ConsumerRecord<String, String> message, Acknowledgment acknowledgment) throws JsonProcessingException {
 		// Todo) 메시지에서 우리가 정의한 메시지타입으로 받게 설정
@@ -47,19 +53,26 @@ public class MyConsumer {
 	@KafkaListener(
 		topics = {MY_JSON_TOPIC},
 		groupId = "batch-test-consumer-group",
-		containerFactory = "batchKafkaListenerContainerFactory"
+		containerFactory = "batchKafkaListenerContainerFactory",
+		concurrency = "3"
 	)
-	public void accept(List<ConsumerRecord<String, String>> messages) {
-		/*ObjectMapper objectMapper = new ObjectMapper();
-		System.out.println("[BATCH]****************************");
-		messages.forEach(msg -> {
-			MyMessage myMessage;
-			try {
-				myMessage = objectMapper.readValue(msg.value(), MyMessage.class);
-				System.out.println(myMessage.toString());
-			} catch (Exception e) {
-				System.out.println(e.toString());
-			}
-		});*/
+	public void accept(List<ConsumerRecord<String, String>> messages, Acknowledgment acknowledgment) {
+		ObjectMapper objectMapper = new ObjectMapper();
+		System.out.println("[BATCH]****************************" );
+		messages.forEach(msg -> executorService.submit(() ->  {
+				MyMessage myMessage;
+				try {
+					myMessage = objectMapper.readValue(msg.value(), MyMessage.class);
+					System.out.println(myMessage.toString());
+				} catch (Exception e) {
+					System.out.println(e.toString());
+				}
+
+				System.out.println("[BATCH]" + Thread.currentThread().getId() + " / " + msg.partition());
+			})
+		);
+
+		// 수동커밋
+		acknowledgment.acknowledge();
 	}
 }
